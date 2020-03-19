77c5d63c7eb3710d47dd8e9050e65df3b21b2bd0
Сергей <nyos@rambler.ru>
2020-03-19 10:15:30 +0500

    1.0.7

--- 1/didrive/twig.function.php
+++ 1/didrive/twig.function.php
@@ -9,15 +9,136 @@
 // });
 // $twig->addFunction($function);
 
+$function = new Twig_SimpleFunction('get_buh_PM_cfg', function () {
 
-$function = new Twig_SimpleFunction('job_buh__getChecksMinusPlus', function ( $db, $date_start, $date_fin, $sp_on = 'all', $module_jobman = '070.jobman', $module_sp = 'sale_point', $module_send_jobman_to_sp = 'jobman_send_on_sp' ) {
+    $return = [];
+
+    //echo '<div style="text-align:left;" >';
+    //\f\pa(\Nyos\Nyos::$menu[\Nyos\mod\JobDesc::$mod_buh_pm]);
+
+    for ($w = 1; $w <= 20; $w++) {
+
+        $t = 'plus';
+        if (!empty(\Nyos\Nyos::$menu[\Nyos\mod\JobDesc::$mod_buh_pm]['type_' . $t]['item' . $w]) && !empty(\Nyos\Nyos::$menu[\Nyos\mod\JobDesc::$mod_buh_pm]['type_' . $t]['item' . $w . 'val'])) {
+            $return[$t][\Nyos\Nyos::$menu[\Nyos\mod\JobDesc::$mod_buh_pm]['type_' . $t]['item' . $w . 'val']] = \Nyos\Nyos::$menu[\Nyos\mod\JobDesc::$mod_buh_pm]['type_' . $t]['item' . $w];
+        }
+
+        $t = 'minus';
+        if (!empty(\Nyos\Nyos::$menu[\Nyos\mod\JobDesc::$mod_buh_pm]['type_' . $t]['item' . $w]) &&
+                !empty(\Nyos\Nyos::$menu[\Nyos\mod\JobDesc::$mod_buh_pm]['type_' . $t]['item' . $w . 'val'])) {
+            $return[$t][\Nyos\Nyos::$menu[\Nyos\mod\JobDesc::$mod_buh_pm]['type_' . $t]['item' . $w . 'val']] = \Nyos\Nyos::$menu[\Nyos\mod\JobDesc::$mod_buh_pm]['type_' . $t]['item' . $w];
+        }
+    }
+
+    return $return;
+});
+$twig->addFunction($function);
+
+$function = new Twig_SimpleFunction('job_buh__get_buh_PM', function ($db, $date, $user) {
+
+    // если нет переменной то не пишем кеш            
+    //$cash_var = 'jobdesc__money_mp_buh__' . \Nyos\mod\JobDesc::$mod_buh_pm . '_jm' . $user . '_date' . $date;
+    // если не указали время жизни то оно бесконечно    
+    // $cash_time_sec = 3600*24;
+    // если есть таймер то показываем таймер выполнения
+    //$show_timer = rand(0, 9999);
+
+    if (!empty($cash_var) && !empty($cash_var) ) {
+
+        if (!empty($show_timer)) {
+            echo '<br/>#' . __LINE__ . ' var ' . $cash_var;
+            \f\timer_start($show_timer);
+        }
+
+        $return = \f\Cash::getVar($cash_var);
+    }
+
+    if (!empty($return)) {
+
+        if (!empty($show_timer))
+            echo '<br/>#' . __LINE__ . ' данные из кеша';
+
+        if (isset($return[0]) && $return[0] == 'mc_skip') {
+            if (!empty($show_timer))
+                echo '<br/>#' . __LINE__ . ' данных нет, возвращаем null';
+            unset($return);
+        }
+    } else {
+
+        $return = [];
+
+        if (!empty($show_timer))
+            echo '<br/>#' . __LINE__ . ' считаем данные и пишем в кеш';
+
+        // тут супер код делающий $return старт
+
+        \Nyos\mod\items::$join_where = ' INNER JOIN `mitems-dops` mid '
+                . ' ON mid.id_item = mi.id '
+                . ' AND mid.name = \'date\' '
+                . ' AND mid.value_date = :d '
+                . ' INNER JOIN `mitems-dops` mid2 '
+                . ' ON mid2.id_item = mi.id '
+                . ' AND mid2.name = \'jobman\' '
+                . ' AND mid2.value = :jm '
+        ;
+
+        \Nyos\mod\items::$var_ar_for_1sql[':jm'] = $user;
+
+        // $ds = date('Y-m-01', strtotime($date));
+        \Nyos\mod\items::$var_ar_for_1sql[':d'] = date('Y-m-01', strtotime($date));
+
+        $ee = \Nyos\mod\items::get($db, \Nyos\mod\JobDesc::$mod_buh_pm);
+        // \f\pa($ee);
+        // $df = date('Y-m-d', strtotime($ds . ' +1 month -1 day'));
+
+        foreach ($ee as $k => $v) {
+            $return[$v['type_minus'] ?? $v['type_plus'] ?? '-'] = 
+                    [ 
+                        'id' => $v['id']
+                        ,'summa' => $v['summa'] 
+                    ];
+        }
+
+        // \f\pa($return);
+        
+//    for ($er = 0; $er <= 32; $er++) {
+//        $nd = date('Y-m-d', strtotime($ds . ' +' . $er . 'day'));
+//
+//        if ($nd > $df)
+//            break;
+//        
+//    }
+//
+//    return $ee;
+        // тут супер код делающий $return конец
+
+        if (!empty($return)) {
+            if (!empty($cash_var))
+            \f\Cash::setVar($cash_var, $return, ( $cash_time_sec ?? 0));
+        } else {
+            if (!empty($cash_var))
+            \f\Cash::setVar($cash_var, [0 => 'mc_skip'], ( $cash_time_sec ?? 0));
+            if (!empty($show_timer))
+                echo '<br/>#' . __LINE__ . ' нет данных ничего не пишем в кеш';
+        }
+    }
+
+    if (!empty($show_timer))
+        echo '<br/>#' . __LINE__ . ' ' . \f\timer_stop($show_timer);
+
+    return $return ?? [];
+    //return \Nyos\mod\JobBuh::getChecksMinusPlus($db, $date_start, $date_fin, $sp_on);
+});
+$twig->addFunction($function);
+
+$function = new Twig_SimpleFunction('job_buh__getChecksMinusPlus', function ($db, $date_start, $date_fin, $sp_on = 'all', $module_jobman = '070.jobman', $module_sp = 'sale_point', $module_send_jobman_to_sp = 'jobman_send_on_sp') {
 
     return \Nyos\mod\JobBuh::getChecksMinusPlus($db, $date_start, $date_fin, $sp_on);
 });
 $twig->addFunction($function);
 
 
-$function = new Twig_SimpleFunction('job_buh__jobman_on_sp', function ( $db, string $date_start, string $date_finish ) {
+$function = new Twig_SimpleFunction('job_buh__jobman_on_sp', function ($db, string $date_start, string $date_finish) {
 
     return \Nyos\mod\JobDesc::getSetupJobmanOnSp($db, $date_start, $date_finish);
 });
@@ -27,7 +148,7 @@ $twig->addFunction($function);
 /**
  * получаем сотрудников кто работал в указанном промежутке
  */
-$function = new Twig_SimpleFunction('job_buh__getJobmansOnTime', function ( $db, string $date_start, string $date_finish ) {
+$function = new Twig_SimpleFunction('job_buh__getJobmansOnTime', function ($db, string $date_start, string $date_finish) {
 
     return \Nyos\mod\JobDesc::getJobmansOnTime($db, $date_start, $date_finish);
 });
@@ -37,10 +158,9 @@ $twig->addFunction($function);
 /**
  * получаем список платежей по чекинам
  */
-$function = new Twig_SimpleFunction('job_buh__get_payed', function ( $db, string $date_start, string $date_finish ) {
-    
+$function = new Twig_SimpleFunction('job_buh__get_payed', function ($db, string $date_start, string $date_finish) {
+
 //    echo '<br/>'.__FILE__.' #'.__LINE__;
-    
     // \Nyos\mod\items::$show_sql = true;
     \Nyos\mod\items::$join_where = ' INNER JOIN `mitems-dops` midop ON '
             . ' midop.name = \'date\' '
@@ -48,21 +168,21 @@ $function = new Twig_SimpleFunction('job_buh__get_payed', function ( $db, string
             . ' AND midop.value_date >= \'' . date('Y-m-d', strtotime($date_start)) . '\' '
             . ' AND midop.value_date <= \'' . date('Y-m-d', strtotime($date_finish)) . '\' '
     ;
-    $d = \Nyos\mod\items::getItemsSimple3($db, '075.buh_oplats',null,'date_asc');
+    $d = \Nyos\mod\items::getItemsSimple3($db, '075.buh_oplats', null, 'date_asc');
 
     // \f\pa($d,'','','d');
-    
-    $r = [ 
+
+    $r = [
 //        'checkins' => [], 
 //        'payed' => [], 
 //        'other' => []
-        ];
+    ];
 
     foreach ($d as $k => $v) {
 
         if (isset($v['checkin'])) {
             $r['checkins'][$v['checkin']][] = $v;
-        } elseif (isset($v['date']) && isset($v['user_id']) ) {
+        } elseif (isset($v['date']) && isset($v['user_id'])) {
             $r['payed'][$v['user_id']][$v['date']][] = $v;
         } else {
             $r['other'][] = $v;
@@ -82,7 +202,7 @@ $twig->addFunction($function);
 
 
 
-$function = new Twig_SimpleFunction('job_buh__getSalarisNow', function ( $db, $sp, $dolgn, $date, $ocenka ) {
+$function = new Twig_SimpleFunction('job_buh__getSalarisNow', function ($db, $sp, $dolgn, $date, $ocenka) {
 // echo '$function = new Twig_SimpleFunction(\'job_buh__getSalarisNow\', function ( $db, '.$sp.', '.$dolgn.', '.$date.', '.$ocenka.' )';
 
     echo '<br/>return \Nyos\mod\JobDesc::getSalarisNow( $db, ' . $sp . ', ' . $dolgn . ', ' . $date . ', ' . $ocenka . ' );';
